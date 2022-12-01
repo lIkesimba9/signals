c599a506a37fcc72b4986e47e215345d235c5885
simba9 <ostilia@mail.ru>
2022-12-01 19:49:21 +0300

    fix2

--- detect_ofdm/main.cpp
+++ detect_ofdm/main.cpp
@@ -6,6 +6,7 @@
 #include <list>
 #include <cstdio>
 #include <stdexcept>
+#include <utility>
 
 using namespace std;
 using namespace complex_literals;
@@ -30,116 +31,110 @@ ifstream & operator >> (ifstream& in, raw_pcm_sample& raw) {
 }
 
 
-class SlidingWindows {
-    size_t size_windows_;
+class Reader {
+    size_t block_size_;
     ifstream input_;
-    vector<complex<double>> data_;
-    bool first_next_;
-    bool flag_last_;
+    bool next_flag_;
+    long long block_num_;
+    
+    void read_block(vector<complex<double>> &block, size_t pos, size_t len) {
+        block_num_++;
+        for (size_t i = pos; i < len; ++i) {
+            raw_pcm_sample raw;
+            if (input_ >> raw) {
+                block[i] = raw;
+            }
+            
+            else {
+                //cout << "test " << block_num_ << '\n';
+                block[i] = 0;
+                next_flag_ = false;
+            }
+        }
+    }
 public:
-    class iterator {
-        friend class SlidingWindows;
-    private:
-
-    public:
-        iterator();
-    };
-    SlidingWindows(string filename, size_t size_windows) : size_windows_(size_windows) {
+    Reader(string filename, size_t block_size) : block_size_(block_size) {
         
         
         input_.open(filename, std::ios::binary);
         if (!input_.is_open()) {
             throw std::invalid_argument( "error open file" );
         }
-        flag_last_ = false;
-        first_next_ = true;
-        for (size_t i = 0; i < size_windows_; ++i) {
-            if (!input_.eof()) {
-                    raw_pcm_sample raw;
-                    input_ >> raw;
-                    data_.push_back(raw);
-            }
-
-            else {
-                flag_last_ = true;
-            }
-        }
-
-        if (data_.size() != size_windows) {
-            throw std::invalid_argument("small file, or biggest windows size!");
-        }
+        next_flag_ = true;
+        block_num_=0;
+        //cout << "construcotr " << block_num_ << '\n';
     }
-    ~SlidingWindows() {
+    ~Reader() {
         if (!input_.is_open()) {
             input_.close();
         }
     }
-    vector<complex<double>> next() {
-        if (first_next_) {
-            first_next_ = false;
-            return data_;
-        }
-        if (flag_last_) {
-            return {};
-        }
-        copy(data_.begin() + size_windows_ / 2, data_.end(), data_.begin());
-        for (size_t i = 0; i < size_windows_ / 2; ++i) {
-            if (!input_.eof()) {
-                raw_pcm_sample raw;
-                input_ >> raw;
-                data_[i + size_windows_ / 2] = raw;
-            }
-            
-            else {
-                data_[i + size_windows_ / 2] = 0;
-                flag_last_ = true;
-            }
-        }
+    vector<complex<double>> init() {
+        vector<complex<double>> block(block_size_);
+        read_block(block, 0, block_size_);
+        //cout << "after init " << block_num_ << '\n';
+        return block;
+    }
 
-        return data_;
+    bool next(vector<complex<double>> &block) {
+        //cout << "before read " << block_num_ << '\n';
+        copy(block.begin() +  block_size_ / 2 + 1, block.end(), block.begin());
+        read_block(block, block_size_ / 2 - 1, block_size_ / 2 + 1);
+        //cout << "after read " << block_num_ << '\n';
+        return next_flag_;
     }
 };
 
 
 
 vector<double> compute_corr(vector<complex<double>>& data, size_t sinc_size, size_t data_size) {
-    size_t i = 0, j = data_size;
+    size_t i = 0;
+    complex<double> value_corr = 0;
     vector<double> corr;
-    for (; j < data.size() - sinc_size; ++j, ++i) {
-        complex<double> value_corr = 0;
-        for (size_t k = 0; k < sinc_size; ++k) {
-            value_corr += (data[i + k] * conj(data[j + k]));
-        }
+    
+    for (; i < sinc_size; ++i) {
+        value_corr += (data[i] * conj(data[data_size + i]));
+    }
+    corr.push_back(value_corr.real());
+
+    for (size_t j = data_size + sinc_size; j < data.size(); ++j, ++i) {
+        value_corr = value_corr - data[i - sinc_size] * conj(data[j - sinc_size]);
+        value_corr = value_corr + data[i] * conj(data[j]);
         corr.push_back(value_corr.real());
     }
     return corr;
 }
 
-void dump_to_csv(vector<double> & vec, string filename = "out.csv") {
-    ofstream out;
-    out.open(filename);
-    if (!out.is_open())
-        return;
-    for (double value : vec) {
-        out << value << '\n';
-    }
-    out.close();
-}
+
 
 int main()
 {
-    string filename = "/Users/simba9/Desktop/signals/detect_ofdm/signal.pcm";
-    size_t size_windows = 1152 * 2;
-    SlidingWindows winwdow_generator(filename, size_windows);
-    vector<complex<double>> window = winwdow_generator.next();
+    string filename = "../signal.pcm";
+    size_t size_block = 1152 * 2;
+    Reader block_generator(filename, size_block);
+    vector<complex<double>> block = block_generator.init();
+    
     vector<double> all_corr_value;
-    while (window.size() > 0)
+    auto corr_win = compute_corr(block, 128, 1024);
+    //cout << "one " << corr_win.size() << '\n';
+    all_corr_value.insert(all_corr_value.end(), corr_win.begin(), corr_win.end());
+    int i = 0;
+    ofstream out;
+    out.open(filename);
+    for (double value : corr_win) 
+            out << value << '\n';
+    while (block_generator.next(block))
     {
-        auto corr_win = compute_corr(window, 128, 1024);
-        all_corr_value.insert(all_corr_value.begin(), corr_win.begin(), corr_win.end());
-        window = winwdow_generator.next();
+        corr_win = compute_corr(block, 128, 1024);
+        //std::copy(corr_win.begin(), corr_win.end(), std::back_inserter(all_corr_value));
+        i++;
+        //cout << i << "\n";
+        for (double value : corr_win) 
+            out << value << '\n';
+
+        
     }
-    dump_to_csv(all_corr_value);
+    cout << "len iter " << i << " " << all_corr_value.size() << '\n';
 
     return 0;
 
