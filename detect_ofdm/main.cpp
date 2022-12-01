a86f605e965bd4dffb4e2500b41728263563cb39
simba9 <ostilia@mail.ru>
2022-12-01 20:14:26 +0300

    test

--- detect_ofdm/main.cpp
+++ detect_ofdm/main.cpp
@@ -35,10 +35,9 @@ class Reader {
     size_t block_size_;
     ifstream input_;
     bool next_flag_;
-    long long block_num_;
     
     void read_block(vector<complex<double>> &block, size_t pos, size_t len) {
-        block_num_++;
+      
         for (size_t i = pos; i < len; ++i) {
             raw_pcm_sample raw;
             if (input_ >> raw) {
@@ -46,7 +45,6 @@ class Reader {
             }
             
             else {
-                //cout << "test " << block_num_ << '\n';
                 block[i] = 0;
                 next_flag_ = false;
             }
@@ -61,8 +59,6 @@ public:
             throw std::invalid_argument( "error open file" );
         }
         next_flag_ = true;
-        block_num_=0;
-        //cout << "construcotr " << block_num_ << '\n';
     }
     ~Reader() {
         if (!input_.is_open()) {
@@ -72,15 +68,12 @@ public:
     vector<complex<double>> init() {
         vector<complex<double>> block(block_size_);
         read_block(block, 0, block_size_);
-        //cout << "after init " << block_num_ << '\n';
         return block;
     }
 
     bool next(vector<complex<double>> &block) {
-        //cout << "before read " << block_num_ << '\n';
         copy(block.begin() +  block_size_ / 2 + 1, block.end(), block.begin());
         read_block(block, block_size_ / 2 - 1, block_size_ / 2 + 1);
-        //cout << "after read " << block_num_ << '\n';
         return next_flag_;
     }
 };
@@ -88,10 +81,11 @@ public:
 
 
 vector<double> compute_corr(vector<complex<double>>& data, size_t sinc_size, size_t data_size) {
-    size_t i = 0;
+    
     complex<double> value_corr = 0;
     vector<double> corr;
     
+    size_t i = 0;
     for (; i < sinc_size; ++i) {
         value_corr += (data[i] * conj(data[data_size + i]));
     }
@@ -109,33 +103,30 @@ vector<double> compute_corr(vector<complex<double>>& data, size_t sinc_size, siz
 
 int main()
 {
-    string filename = "../signal.pcm";
+    string filename = "../signal.pcm"; //"/Users/simba9/Desktop/signals/detect_ofdm/signal.pcm";
     size_t size_block = 1152 * 2;
     Reader block_generator(filename, size_block);
     vector<complex<double>> block = block_generator.init();
     
     vector<double> all_corr_value;
     auto corr_win = compute_corr(block, 128, 1024);
-    //cout << "one " << corr_win.size() << '\n';
     all_corr_value.insert(all_corr_value.end(), corr_win.begin(), corr_win.end());
     int i = 0;
     ofstream out;
-    out.open(filename);
+    out.open("out.csv");
     for (double value : corr_win) 
             out << value << '\n';
     while (block_generator.next(block))
     {
         corr_win = compute_corr(block, 128, 1024);
-        //std::copy(corr_win.begin(), corr_win.end(), std::back_inserter(all_corr_value));
         i++;
-        //cout << i << "\n";
         for (double value : corr_win) 
             out << value << '\n';
 
         
     }
     cout << "len iter " << i << " " << all_corr_value.size() << '\n';
-
+    out.close();
     return 0;
 
 }
